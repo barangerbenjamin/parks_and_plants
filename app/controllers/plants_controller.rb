e2f9d653847256606dcee996d6474a0f5bf9ad4e
Benjamin Baranger <baranger.benjamin@gmail.com>
2018-11-07 18:34:31 +0000

    garden and plants

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,31 @@
+class PlantsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    @plant = Plant.new(plant_params)
+    @garden = Garden.find(params[:garden_id])
+    @plant.garden = @garden
+    if @plant.save
+      redirect_to garden_path(@garden)
+    else
+      render "gardens/show"
+    end
+  end
+
+  def destroy
+    @plant = Plant.find(params[:id])
+    @garden = @plant.garden
+    @plant.destroy
+    redirect_to garden_path(@garden)
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:name, :image_url)
+  end
+
+end
